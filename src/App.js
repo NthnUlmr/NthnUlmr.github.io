771a2f4af92d370abe7dd6f11aa4ddaeb6a690dd
Nathan <quaz990@gmail.com>
2024-01-07 16:06:19 -0700

    parallax working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {Parallax, ParallaxLayer } from '@react-spring/parallax'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parallax pages={2}>
+        <ParallaxLayer offset={0} speed={1} factor={1} style={{
+            backgroundImage: 'url(./parallax/mainParallax_sky.png)',
+            backgroundSize: 'cover',
+          }}>
+          <h2>Welcome</h2>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={1.5} factor={1} style={{
+            backgroundImage: 'url(./parallax/mainParallax_background.png)',
+            backgroundSize: 'cover',
+          }}>
+
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={2.0} factor={1} style={{
+            backgroundImage: 'url(./parallax/mainParallax_farMidground.png)',
+            backgroundSize: 'cover',
+          }}>
+
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={2.5} factor={1} style={{
+            backgroundImage: 'url(./parallax/mainParallax_closeMidground.png)',
+            backgroundSize: 'cover',
+          }}>
+
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={2.5} factor={1} style={{
+            backgroundImage: 'url(./parallax/mainParallax_foreground.png)',
+            backgroundSize: 'cover',
+          }}>
+
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={100.1}>
+          <h2> Go Away</h2>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
