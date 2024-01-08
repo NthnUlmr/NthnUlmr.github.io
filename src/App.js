7557eef81344aaaeb201f985bc655c59ccb66029
Nathan <quaz990@gmail.com>
2024-01-07 21:07:56 -0700

    Starting to add content. I need to look into why the text won't format correctly with the other elements.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import {Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 function App() {
   return (
-    <div className="App" style={{backgroundImage: 'url(./parallax/mainParallax_sky.png)'}}>
-      <Parallax pages={2}>
-        <ParallaxLayer offset={0} speed={0} factor={1.5} style={{
+    <div className="App">
+      <Parallax pages={5}>
+        <ParallaxLayer offset={0} speed={0.1} factor={1.5} style={{
             backgroundImage: 'url(./parallax/mainParallax_sky.png)',
             backgroundSize: 'cover',
             backgroundPositionX: '50%'
@@ -46,13 +46,30 @@ function App() {
 
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={5.5} factor={1.} sticky={{start: 1.0, end: 100}} style={{
+        <ParallaxLayer offset={0.0} speed={1000} factor={0} sticky={{start: 1.0, end: 100}} style={{
             backgroundImage: 'url(./profile2.png)',
             backgroundSize: '10%',
-            backgroundPositionX: '1%',
+            backgroundPositionX: '50%',
             backgroundPositionY: '1%'
           }}>
-          <h1>Nathan Ulmer GRAPHIC DESIGN IS MY PASSION</h1>
+          
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={0.0} speed={0.1} factor={0} sticky={{start: 1.5, end: 3}} style={{
+            backgroundSize: '10%',
+            backgroundPositionX: '50%',
+            backgroundPositionY: '50%'
+          }}>
+          
+          <h1 style={{backgroundPositionY:"10%"}}>Nathan Ulmer GRAPHIC DESIGN IS MY PASSION</h1>
+          <p> I don't know how to make text go where I want. All images used are either pictures I've taken or things I've drawn in GIMP.</p>
+          <h2> Photography // Graphics // Video Games // Physics Simulations // Writing</h2>
+
+          <h2>Project 1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h2>Project 2</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
           </ParallaxLayer>
 
 
